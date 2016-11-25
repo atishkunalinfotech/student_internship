a055de7a19bf1f98b05247cec4129381ecd9e8ba
Atish <atishkumarlewate@gmail.com>
2016-11-24 21:36:29 -0500

    migration file removed

--- db/migrate/20160326062612_add_description_to_jobs.rb
+++
@@ -1,8 +0,0 @@
-class AddDescriptionToJobs < ActiveRecord::Migration
-  def change
-    add_column :jobs, :description, :text
-    add_column :jobs, :responsibilities, :text
-    add_column :jobs, :requirements, :text
-    add_column :jobs, :salary, :string
-  end
-end
